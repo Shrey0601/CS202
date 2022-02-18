c3d98e2e1319b259ddeda85d0617e96637436b65
Kushalg99 <87751009+Kushalg99@users.noreply.github.com>
2022-02-18 10:31:03 +0530

    Add files via upload

--- Assignment 2/SatSolver.py
+++ Assignment 2/SatSolver.py
@@ -15,18 +15,20 @@ for i in range(1,n_lit+1):
 
 
 def sat_solver(cnf):
+    if len(cnf) == 0:
+        return 1
     temp_true=[]
     temp_false=[]
     clauses=[]
 
     for clause in cnf:
         if len(clause) == 1:
-            clauses.append(clause)
+            clauses.append(clause[0])
             cnf.remove(clause)
 
     for clause in clauses :
         if(clause < 0):
-            model_dict[clause] = -1
+            model_dict[abs(clause)] = -1
             temp_false.append(clause)
         else:
             model_dict[clause] = 1
@@ -44,24 +46,23 @@ def sat_solver(cnf):
                 if(len(clause) == 0):
                     return 0
                 break
+    return sat_solver(cnf)
 
-    if len(cnf) == 0:
-        return 1
-
-    for x in list:
-        cnf1 = deepcopy(cnf) + [x]
-        cnf2 = deepcopy(cnf) + [-x]
-        if(sat_solver(sat_solver(cnf1))):
-            return 1
-        if(sat_solver(sat_solver(cnf2))):
-            return 1
-    return 0
+    # for x in list:
+    #     cnf1 = deepcopy(cnf) + [x]
+    #     cnf2 = deepcopy(cnf) + [-x]
+    #     if(sat_solver(cnf1)):
+    #         return 1
+    #     if(sat_solver(cnf2)):
+    #         return 1
+    # return 0
 
 
-def dpll(cnf):
-    #Take inputs 
-    #Convert it to literals
-    #solve cnf
-    cnf = [ [1], [2], [1,2]]
-    print(sat_solver(cnf))
-    return
+
+# cnf = []
+# with open("uf20-011.cnf") as f:
+#     for line in f:
+#         if not (line.startswith("c") or line.startswith("p")):
+#             cnf.append([int(x) for x in line.rstrip("0").rstrip("0\n").split()])
+cnf = [[1,2], [2,3]]
+print(sat_solver(cnf))
